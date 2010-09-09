836d7397d9d2a77029aa61aefeaaefb3e4b23c1f
Javier Hernández Antúnez <jhernandez@emergya.com>
2010-09-09 13:07:51 +0000

    Guaduelo now runs from the source or installed

--- lib/loader.py
+++ lib/loader.py
@@ -2,16 +2,22 @@ import pygame
 from pygame.locals import *
 
 from os import path
+
+if path.exists('/usr/share/guaduelo'):
+    BASE_DIR = '/usr/share/guaduelo/data'
+else:
+    BASE_DIR = './data'
+
 class Loader(object):
     def __init__(self):
         pass
 
     def load_sound(self,filename):
-        filepath = path.join("data","snd",filename)
+        filepath = path.join(BASE_DIR,"snd",filename)
         return pygame.mixer.Sound(filepath)
 
     def load_image(self,filename,alpha=False):
-        filepath = path.join("data","img",filename)
+        filepath = path.join(BASE_DIR,"img",filename)
         img = pygame.image.load(filepath)
         if alpha:
             img = img.convert_alpha()
@@ -20,6 +26,6 @@ class Loader(object):
         return img        
 
     def load_font(self,filename,size):
-        filepath = path.join("data","fnt",filename)
+        filepath = path.join(BASE_DIR,"fnt",filename)
         fnt = pygame.font.Font(filepath, size)
         return fnt  
